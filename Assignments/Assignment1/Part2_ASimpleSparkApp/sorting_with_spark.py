0f16e10ec8b688bc5053abc86e3708454ec55bba
Rohit <rohit.sharma@wisc.edu>
2019-09-18 12:32:59 -0500

    Added some comments to the part 2 code

--- Assignments/Assignment1/Part2_ASimpleSparkApp/sorting_with_spark.py
+++ Assignments/Assignment1/Part2_ASimpleSparkApp/sorting_with_spark.py
@@ -1,16 +1,21 @@
-from pyspark.sql.functions import col
-from pyspark import SparkContext, SparkConf 
 import sys
+from pyspark import SparkContext, SparkConf 
 
+
+# Input and output file name from the command line
 input_filename = sys.argv[1]
 output_filename = sys.argv[2]
 
+# Creating a Spark context object using the configuration specified below
 conf = SparkConf().setAppName("Part2_SortingWithSpark").setMaster("local")
 sc = SparkContext(conf=conf)
 
+# Load the input file as an RDD
+lines = sc.textFile(input_filename)
 
-dataframe = sc.textFile(input_filename)
+# Sort based on country code (3rd column) first and then timestamp (last column) with sortBy transformation
+# TODO: Ignore the header in sorting and append the header as first line in the output file?
+sorted_lines = lines.sortBy(lambda x: (x.split(",")[2], x.split(",")[-1]))
 
-sorted_dataframe = dataframe.sortBy(lambda x: (x.split(",")[2], x.split(",")[-1]))
-
-sorted_dataframe.saveAsTextFile(output_filename)
+# Write the output to disk with saveAsTextFile action
+sorted_lines.saveAsTextFile(output_filename)
