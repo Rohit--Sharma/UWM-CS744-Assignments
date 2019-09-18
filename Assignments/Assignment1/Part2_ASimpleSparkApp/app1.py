4207d4d887a7975e1cb7b41ff90bb5658f17a364
Akshata Bhat <akshatabhat@Akshatas-MacBook-Pro-2.local>
2019-09-18 12:20:02 -0500

    sorting

--- Assignments/Assignment1/Part2_ASimpleSparkApp/app1.py
+++ Assignments/Assignment1/Part2_ASimpleSparkApp/app1.py
@@ -1,13 +1,16 @@
 from pyspark.sql.functions import col
 from pyspark import SparkContext, SparkConf 
+import sys
 
+input_filename = sys.argv[0]
+output_filename = sys.arg[1]
 
 conf = SparkConf().setAppName("Part2_SortingWithSpark").setMaster("local")
 sc = SparkContext(conf=conf)
 
 
-dataframe = sc.textFile("export.csv")
+dataframe = sc.textFile(input_filename)
 
 sorted_dataframe = dataframe.sortBy(lambda x: (x.split(",")[2], x.split(",")[-1]))
 
-sorted_dataframe.saveAsTextFile("export_1.csv")
+sorted_dataframe.saveAsTextFile(output_filename)
