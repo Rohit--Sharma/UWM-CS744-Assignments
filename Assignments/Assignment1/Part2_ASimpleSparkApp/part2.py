cb6d89d8953e2bd47fec1cb9cb0b45a59eb95a13
Rohit <rohit.sharma@wisc.edu>
2019-09-18 11:16:17 -0500

    Counting number of lines in file

--- Assignments/Assignment1/Part2_ASimpleSparkApp/part2.py
+++ Assignments/Assignment1/Part2_ASimpleSparkApp/part2.py
@@ -1,11 +1,13 @@
 from pyspark import SparkContext, SparkConf 
 
 # The first thing that Spark application does is creating an object SparkContext.
-conf = SparkConf().setAppName(appName).setMaster(master)
+conf = SparkConf().setAppName("Part2_SortingWithSpark").setMaster("local")
 sc = SparkContext(conf=conf)
 
 # You can read the data from file into RDD object as a collection of lines
-lines = sc.textFile("data.txt")
+lines = sc.textFile("export.csv")
 
 lineLengths = lines.map(lambda s: len(s))
 totalLength = lineLengths.reduce(lambda a, b: a + b)
+
+print(totalLength)
