7cee8147a23afda17a588ba1360adea11d7a9b78
Akshata Bhat <mail.akshata18@gmail.com>
2019-09-18 11:45:30 -0500

    fixing bug

---
+++ Assignments/Assignment1/Part2_ASimpleSparkApp/app1.py
@@ -0,0 +1,11 @@
+from pyspark.sql.functions import col
+from pyspark import SparkContext, SparkConf 
+
+
+conf = SparkConf().setAppName("Part2_SortingWithSpark").setMaster("local")
+sc = SparkContext(conf=conf)
+
+
+datafram = sc.textFile("export.csv")
+
+dataframe.sortByKey('cca2')
