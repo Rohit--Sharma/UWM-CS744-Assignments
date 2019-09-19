17f366551e93208106f219372535a92c5c07e35d
akshatabhat <mail.akshata18@gmail.com>
2019-09-19 11:16:14 -0500

    minor modification

--- Assignments/Assignment1/Part3_PageRank/pagerank.py
+++ Assignments/Assignment1/Part3_PageRank/pagerank.py
@@ -1,1 +1,7 @@
 from pyspark import SparkContext, SparkConf 
+
+conf = SparkConf().setAppName("Part3_PageRank").setMaster("local")
+sc = SparkContext(conf=conf)
+
+#documents = sc.textFile("/proj/uwmadison744-f19-PG0/data-part3/enwiki-pages-articles/")
+documents = sc.textFile("web-BerkStan.txt")
