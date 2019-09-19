89dac2399f40db556e0372c25f9c2b0c338a1be5
root <root@node0.rsharma-asgn01.uwmadison744-f19-pg0.wisc.cloudlab.us>
2019-09-19 11:24:20 -0500

    removed header from csv file before sorting

--- Assignments/Assignment1/Part2_ASimpleSparkApp/sorting_with_spark.py
+++ Assignments/Assignment1/Part2_ASimpleSparkApp/sorting_with_spark.py
@@ -13,10 +13,12 @@ sc = SparkContext(conf=conf)
 # Load the input file as an RDD
 # TODO: Load the data into HDFS and load the file from HDFS using "hdfs://..."
 lines = sc.textFile(input_filename)
+header = lines.first()
+filtered_lines = lines.filter(lambda line: line != header)
 
 # Sort based on country code (3rd column) first and then timestamp (last column) with sortBy transformation
 # TODO: Ignore the header in sorting and append the header as first line in the output file?
-sorted_lines = lines.sortBy(lambda x: (x.split(",")[2], x.split(",")[-1]))
+sorted_lines = filtered_lines.sortBy(lambda x: (x.split(",")[2], x.split(",")[-1]))
 
 # Write the output to disk with saveAsTextFile action
 sorted_lines.saveAsTextFile(output_filename)
