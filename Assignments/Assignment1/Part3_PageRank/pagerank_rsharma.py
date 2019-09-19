7289c686e9d148844e7a4785a1f1745435a615eb
Rohit <rohit.sharma@wisc.edu>
2019-09-19 12:09:06 -0500

    Initial draft of my page rank implementation using Spark

--- Assignments/Assignment1/Part3_PageRank/pagerank_rsharma.py
+++ Assignments/Assignment1/Part3_PageRank/pagerank_rsharma.py
@@ -1,3 +1,4 @@
+from operator import add
 from pyspark import SparkContext, SparkConf 
 
 conf = SparkConf().setAppName("Part3_PageRank").setMaster("local")
@@ -5,7 +6,20 @@ sc = SparkContext(conf=conf)
 
 #documents = sc.textFile("/proj/uwmadison744-f19-PG0/data-part3/enwiki-pages-articles/")
 documents = sc.textFile("web-BerkStan.txt")
+n_iter = 10
 
 # Filter the comments beginning with # and create an RDD 
-lines = documents.filter(lambda x: x[0] != '#').map(lambda x: (x.split('\t')[0], x.split('\t')[1]))
+links = documents.filter(lambda x: x[0] != '#').map(lambda x: (x.split('\t')[0], x.split('\t')[1])).persist()
 
+ranks = links.groupByKey().mapValues(lambda x: 1)
+
+links = links.groupByKey().mapValues(len)
+
+for i in range(n_iter):
+    # Build an RDD of (targetURL, float) pairs
+    # with the contributions sent by each page
+    contribs = links.join(ranks).flatMap(lambda x: x[1][0].map(lambda dest: (dest, x[1][1] / len(x[1][0]))))
+    # Sum contributions by URL and get new ranks
+    ranks = contribs.reduceByKey(add).mapValues(lambda sum: 0.15 + 0.85 * sum)
+
+ranks.collect()
