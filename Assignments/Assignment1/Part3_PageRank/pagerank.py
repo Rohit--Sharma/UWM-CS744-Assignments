27871a90170b3fb5d99eef2547dd953918f4eebd
akshatabhat <mail.akshata18@gmail.com>
2019-09-19 12:17:32 -0500

    page rank iterator bug fixes

--- Assignments/Assignment1/Part3_PageRank/pagerank.py
+++ Assignments/Assignment1/Part3_PageRank/pagerank.py
@@ -6,11 +6,11 @@ sc = SparkContext(conf=conf)
 #documents = sc.textFile("/proj/uwmadison744-f19-PG0/data-part3/enwiki-pages-articles/")
 documents = sc.textFile("web-BerkStan.txt")
 
-ranks = lines.groupByKey().mapValues(lambda x : 1)
+ranks = documents.groupByKey().mapValues(lambda x : 1)
 
 ITERATIONS = 10
-for i in to range(ITERATIONS):
+for i in range(ITERATIONS):
 	#Build an RDD of (targetURL, float) pairs // with the contributions sent by each page 
-	contribs = lines.join(ranks).flatMap(lambda url_rank : links.map(lambda url_rank : (url_rank[1][0], url_rank[1][1]/len(url_rank[1]))))
+	contribs = documents.join(ranks).flatMap(lambda url_rank : documents.map(lambda url_rank : (url_rank[1][0], url_rank[1][1]/len(url_rank[1]))))
 	#Sum contributions by URL and get new ranks 
 	ranks = contribs.reduceByKey(lambda x, y : x+y).mapValues(lambda r :  0.15 + 0.85 * r)
