ce01aa1e7935bded35c5eaddb266b8ec87a2907c
akshatabhat <mail.akshata18@gmail.com>
2019-09-19 12:51:24 -0500

    modified to read actual dataset

--- Assignments/Assignment1/Part3_PageRank/pagerank.py
+++ Assignments/Assignment1/Part3_PageRank/pagerank.py
@@ -1,3 +1,4 @@
+from operator import add
 from pyspark import SparkContext, SparkConf 
 
 conf = SparkConf().setAppName("Part3_PageRank").setMaster("local")
@@ -5,12 +6,29 @@ sc = SparkContext(conf=conf)
 
 #documents = sc.textFile("/proj/uwmadison744-f19-PG0/data-part3/enwiki-pages-articles/")
 documents = sc.textFile("web-BerkStan.txt")
+n_iter = 10
 
-ranks = documents.groupByKey().mapValues(lambda x : 1)
+# Filter the comments beginning with # and create an RDD 
+links = documents.filter(lambda x: x[0] != '#').map(lambda x: (x.split('\t')[0], x.split('\t')[1])).persist()
 
-ITERATIONS = 10
-for i in range(ITERATIONS):
-	#Build an RDD of (targetURL, float) pairs // with the contributions sent by each page 
-	contribs = documents.join(ranks).flatMap(lambda url_rank : documents.map(lambda url_rank : (url_rank[1][0], url_rank[1][1]/len(url_rank[1]))))
-	#Sum contributions by URL and get new ranks 
-	ranks = contribs.reduceByKey(lambda x, y : x+y).mapValues(lambda r :  0.15 + 0.85 * r)
+ranks = links.groupByKey().mapValues(lambda x: 1)
+
+links = links.groupByKey().mapValues(list)
+
+def computeContribs(urls, rank):
+    """Calculates URL contributions to the rank of other URLs."""
+    num_urls = len(urls)
+    for url in urls:
+        yield (url, rank / num_urls)
+
+for i in range(n_iter):
+    # Build an RDD of (targetURL, float) pairs
+    # with the contributions sent by each page
+    contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
+    # Sum contributions by URL and get new ranks
+    ranks = contribs.reduceByKey(add).mapValues(lambda sum: 0.15 + 0.85 * sum)
+
+ranks = ranks.sortBy(lambda url_rank: url_rank[1], ascending=False)
+
+ranks_ = ranks.collect()
+print(ranks_[0:100])
