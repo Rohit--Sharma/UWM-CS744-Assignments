d7c307f0fe97e02614b705758cbab53bf8d3e876
akshatabhat <mail.akshata18@gmail.com>
2019-09-22 12:18:18 -0500

    Correcting indentation

--- Assignments/Assignment1/Part3_PageRank/task1/pagerank_enwiki.py
+++ Assignments/Assignment1/Part3_PageRank/task1/pagerank_enwiki.py
@@ -21,19 +21,19 @@ ranks = links.groupByKey().mapValues(lambda x: 1)
 links = links.groupByKey().mapValues(list)
 
 def computeContribs(urls, rank):
-    """Calculates URL contributions to the rank of other URLs."""
-    num_urls = len(urls)
-    for url in urls:
-        yield (url, rank / num_urls)
+	"""Calculates URL contributions to the rank of other URLs."""
+	num_urls = len(urls)
+	for url in urls:
+		yield (url, rank / num_urls)
 
-for i in range(n_iter):
-    # Build an RDD of (targetURL, float) pairs
-    # with the contributions sent by each page
-    contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
-    # Sum contributions by URL and get new ranks
-    ranks = contribs.reduceByKey(add).mapValues(lambda sum: 0.15 + 0.85 * sum)
+	for i in range(n_iter):
+		# Build an RDD of (targetURL, float) pairs
+		# with the contributions sent by each page
+		contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
+		# Sum contributions by URL and get new ranks
+		ranks = contribs.reduceByKey(add).mapValues(lambda sum: 0.15 + 0.85 * sum)
 
-# ranks = ranks.sortBy(lambda url_rank: url_rank[1], ascending=False)
+# ranks = ranks.sortBy(lambda url_rank: url_rank[2], ascending=False)
 
 ranks_ = ranks.collect()
-# print(ranks_[0:100])
+#print(ranks_[1:100])
