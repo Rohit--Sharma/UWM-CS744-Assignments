a852906e131a55805a50aede8d2dd77750de8c94
akshatabhat <mail.akshata18@gmail.com>
2019-09-24 21:13:32 -0500

    Code clean up

--- Assignments/Assignment1/Part3_PageRank/task3/pagerank.py
+++ Assignments/Assignment1/Part3_PageRank/task3/pagerank.py
@@ -1,38 +1,60 @@
+import sys
 from operator import add
-from pyspark import SparkContext, SparkConf 
+from pyspark import SparkContext, SparkConf
+
+file_type = sys.argv[1] 
+n_iter = int(sys.argv[2])
+num_partitions = int(sys.argv[3])
+spark_master_hostname = sys.argv[4]
+input_path = sys.argv[5]
+output_path = sys.argv[6]
+
+def filter_func(x, file_type):
+	if file_type == "small":
+		return x[0] != '#'
+	else:
+		if ":" in x[1] and not x[1].startswith("category:"):
+			return False
+		return True
+
+def split_func(x, file_type):
+	if file_type == "small":
+		temp = x.split('\t')
+		return (temp[0], temp[1])
+	else:
+		temp = x.lower().split('\t')
+		return (temp[0], temp[1])
+
+def computeContribs(urls, rank):
+	"""Calculates URL contributions to the rank of other URLs."""
+	num_urls = len(urls)
+	for url in urls:
+		yield (url, rank / num_urls)
 
 conf = SparkConf().setAppName("Part3_PageRank")\
 	.set('spark.executor.memory', '29g')\
 	.set('spark.executor.cores', '5')\
 	.set('spark.driver.memory', '29g')\
 	.set('spark.task.cpus', '1')\
-	.setMaster('spark://' + 'c220g1-031124vm-1.wisc.cloudlab.us' + ':7077')
+	.setMaster('spark://' + spark_master_hostname + ':7077')
 sc = SparkContext(conf=conf)
 
-documents = sc.textFile("web-BerkStan.txt")
-n_iter = 10
+documents = sc.textFile(input_path)
 
 # Filter the comments beginning with # and create an RDD 
-links = documents.filter(lambda x: x[0] != '#').map(lambda x: (x.split('\t')[0], x.split('\t')[1]))
+links = documents.filter(lambda x: filter_func(x, file_type)).map(lambda x: split_func(x, file_type)).groupByKey().partitionBy(num_partitions).cache()
 
-links = links.groupByKey().mapValues(list).persist()
+# Initialise the rank of each to 1
+ranks = links.mapValues(lambda x: 1).partitionBy(num_partitions)
 
-ranks = links.mapValues(lambda x: 1)
-
-def computeContribs(urls, rank):
-    """Calculates URL contributions to the rank of other URLs."""
-    num_urls = len(urls)
-    for url in urls:
-        yield (url, rank / num_urls)
-
+# On each iteration, each page contributes to its neighbors by rank(p)/ # of neighbors.
 for i in range(n_iter):
-    # Build an RDD of (targetURL, float) pairs
-    # with the contributions sent by each page
-    contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
-    # Sum contributions by URL and get new ranks
-    ranks = contribs.reduceByKey(add).mapValues(lambda sum: 0.15 + 0.85 * sum)
+	# Build an RDD of (targetURL, float) pairs
+	# with the contributions sent by each page
+	contribs = links.join(ranks).flatMap(lambda url_urls_rank: computeContribs(url_urls_rank[1][0], url_urls_rank[1][1]))
 
-# ranks = ranks.sortBy(lambda url_rank: url_rank[1], ascending=False)
+	# Sum contributions by URL and get new ranks
+	ranks = contribs.reduceByKey(add).mapValues(lambda sum: 0.15 + 0.85 * sum).partitionBy(num_partitions)
 
-ranks_ = ranks.collect()
-# print(ranks_[0:100])
+
+ranks_ = ranks.saveAsTextFile(output_path)
