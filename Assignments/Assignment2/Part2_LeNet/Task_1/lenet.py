456bfdc0ae20226ee0bfbf7dc56ff71e6b0f2d1e
akshatabhat <mail.akshata18@gmail.com>
2019-10-05 13:59:44 -0500

    Lenet for MNIST

--- Assignments/Assignment2/Part2_LeNet/Task_1/lenet.py
+++ Assignments/Assignment2/Part2_LeNet/Task_1/lenet.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D
 from tensorflow.keras.datasets import mnist
 
 
@@ -14,46 +14,54 @@ model = models.Sequential()
 
 model.add(layers.Conv2D(
 	filters=6, 
-	kernel_size=(5,5), 
-	padding=0,
+	kernel_size=(5,5),
 	stride=1,
+	padding="valid",
+	activation="tanh",
 	input_shape = (32, 32, 1)
 	))
 
-model.add(layes.MaxPooling2D(
-	filters=6,
-	kernel_size=(2,2),
+model.add(layes.AveragePooling2D(
+	pool_size=(2,2),
 	padding=0,
 	stride=2,
-	input_shape= (28, 28, 6)
 	))
 
 model.add(layers.Conv2D(
 	filters=16,
-	kernel_size=(10, 10),
+	kernel_size=(5, 5),
 	stride=1,
-	padding=0
+	padding="valid",
+	activation="tanh",
+	input_shape = (14, 14, 6)
 	))
 
-model.add(layers.MaxPooling2D(
-	filters=16,
-	kernel_size = (5,5),
-	stride=1
+model.add(layers.AveragePooling2D(
+	pool_size = (5,5),
+	stride=2
+	))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(
+	units=120,
+	kernel_size=(1,1),
+	activation="tanh"
 	))
 
 model.add(layers.Dense(
-	filters=120,
-	kernel_size=(1,1)
+	units=84,
+	activation="tanh"
 	))
 
 model.add(layers.Dense(
-	filters=84
+	units=10,
+	activation="softmax"
 	))
 
-model.add(layers.Dense(
-	filters=10
-	))
 
+model.compile(loss='categorical_crossentropy', optimizer='sgd')
+model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 #x = tf.placeholder(tf.float32, shape=[None, 3])
 #linear_model = tf.layers.Dense(units=1)
 #y = linear_model(x)
