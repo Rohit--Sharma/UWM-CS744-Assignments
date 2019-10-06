efc2d3c17b9821a69de691fb44f99714b9d79f1a
akshatabhat <mail.akshata18@gmail.com>
2019-10-06 15:50:05 -0500

    Sync SGD

--- Assignments/Assignment2/Part1_LogisticRegression/Task_2/logistic_regression_sync.py
+++ Assignments/Assignment2/Part1_LogisticRegression/Task_2/logistic_regression_sync.py
@@ -18,22 +18,22 @@ clusterSpec_single = tf.train.ClusterSpec({
 
 clusterSpec_cluster = tf.train.ClusterSpec({
     "ps" : [
-        "node0:2222"
+        "node0.rsharma-assign2.uwmadison744-f19-PG0.wisc.cloudlab.us:2222"
     ],
     "worker" : [
-        "host_name0:2223",
-        "host_name1:2222"
+        "node0.rsharma-assign2.uwmadison744-f19-PG0.wisc.cloudlab.us:2223",
+        "node1.rsharma-assign2.uwmadison744-f19-pg0.wisc.cloudlab.us:2222"
     ]
 })
 
 clusterSpec_cluster2 = tf.train.ClusterSpec({
     "ps" : [
-        "host_name0:2222"
+        "localhost:2222"
     ],
     "worker" : [
-        "host_name0:2223",
-        "host_name1:2222",
-        "host_name2:2222",
+        "localhost:2223",
+        "10.10.1.2:2222",
+        "10.10.1.3:2222",
     ]
 })
 
@@ -79,36 +79,44 @@ def main():
             optimizer = tf.train.SyncReplicasOptimizer(optimizer, replicas_to_aggregate=2,
                                    total_num_replicas=2)
 
-            #init_token_op = opt.get_init_tokens_op()
-            #chief_queue_runner = opt.get_chief_queue_runner()
+            sync_init_op = optimizer.get_init_tokens_op()
+            chief_queue_runner = optimizer.get_chief_queue_runner()
 
             # Initialising the variables.
             init_op = tf.global_variables_initializer()
-            # The StopAtStepHook handles stopping after running given steps.
-            hooks=[tf.train.StopAtStepHook(last_step=1000000)]
-            # The MonitoredTrainingSession takes care of session initialization,
-            # restoring from a checkpoint, saving to a checkpoint, and closing when done
-            # or an error occurs.
+            
             sv = tf.train.Supervisor(is_chief=is_chief,
                 logdir="/tmp/train_logs",
                 init_op=init_op,
                 global_step=global_step,
                 save_model_secs=600)
 
-            with training.MonitoredTrainingSession(
-                master=workers[worker_id].target, is_chief=is_chief,
-                hooks=[sync_replicas_hook]) as mon_sess:
-                mon_sess.run(init)
+            with sv.prepare_or_wait_for_session(server.target) as sess:
+                sess.run(init_op)
+                if is_chief:
+                    sv.start_queue_runners(sess, [chief_queue_runner])
+                    # Insert initial tokens to the queue.
+                    sess.run(sync_init_op)
+                for iter in range(num_iter):
+                        avg_loss = 0
+                        num_batches = int(mnist.train.num_examples/batch_size)
 
-                iter = 0
-                while not mon_sess.should_stop():
-                    data_x, data_y = mnist.train.next_batch(batch_size)
-                    _, loss_val = mon_sess.run((optimizer, loss), feed_dict={x: data_x, y: data_y})
+                        for _ in range(num_batches):
+                            data_x, data_y = mnist.train.next_batch(batch_size)
+                            _, loss_val = sess.run((optimizer, loss), feed_dict={x: data_x, y: data_y})
 
-                    # printing the loss after every iteration (epoch)
-                    if (iter+1) % display_step == 0:
-                        print("Epoch:", '%04d' % (iter+1), "cost=", "{:.9f}".format(loss_val))
-                    iter += 1
+                            avg_loss += loss_val / num_batches
+
+                        # printing the loss after every iteration (epoch)
+                        if (iter+1) % display_step == 0:
+                            print("Epoch:", '%04d' % (iter+1), "loss=", "{:.9f}".format(avg_loss))
+
+                    # Computing the model accuracy
+                    correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+                    # Calculate accuracy on test data
+                    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
 
 
