0896a4e64f7c00fefda3cd915e430a22b978dd9e
Rohit <rohit.sharma@wisc.edu>
2019-10-06 14:29:12 -0500

    Added script name to terminate_cluster script

--- Assignments/Assignment2/Part1_LogisticRegression/Task_2/cluster_utils.sh
+++ Assignments/Assignment2/Part1_LogisticRegression/Task_2/cluster_utils.sh
@@ -3,7 +3,7 @@ export TF_RUN_DIR="~/tf"
 
 function terminate_cluster() {
     echo "Terminating the servers"
-    CMD="ps aux | grep -v 'grep' | grep -v 'bash' | grep -v 'ssh' | grep 'python' | awk -F' ' '{print \$2}' | xargs kill -9"
+    CMD="ps aux | grep -v 'grep' | grep -v 'bash' | grep -v 'ssh' | grep 'python3 logistic_regression' | awk -F' ' '{print \$2}' | xargs kill -9"
     for i in `seq 0 2`; do
         ssh node$i "$CMD"
     done
