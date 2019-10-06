57c32e716396a1b2de587eeb71bb28da9363150b
Rohit <rohit.sharma@wisc.edu>
2019-10-06 14:58:19 -0500

    Async SGD done. Added code to compute train accuracy

--- Assignments/Assignment2/Part1_LogisticRegression/Task_2/logistic_regression_async.py
+++ Assignments/Assignment2/Part1_LogisticRegression/Task_2/logistic_regression_async.py
@@ -90,17 +90,29 @@ def main():
         # Initializing the variables
         init = tf.global_variables_initializer()
 
-        with tf.Session(master=server.target) as sess:
+        with tf.Session(server.target) as sess:
             sess.run(init)
 
             for iter in range(num_iter):
-                data_x, data_y = mnist.train.next_batch(batch_size)
-                _, loss_val = sess.run((optimizer, loss), feed_dict={x: data_x, y: data_y})
+                avg_loss = 0
+                num_batches = int(mnist.train.num_examples/batch_size)
+
+                for _ in range(num_batches):
+                    data_x, data_y = mnist.train.next_batch(batch_size)
+                    _, loss_val = sess.run((optimizer, loss), feed_dict={x: data_x, y: data_y})
+
+                    avg_loss += loss_val / num_batches
 
                 # printing the loss after every iteration (epoch)
                 if (iter+1) % display_step == 0:
-                    print("Epoch:", '%04d' % (iter+1), "loss=", "{:.9f}".format(loss_val))
-                iter += 1
+                    print("Epoch:", '%04d' % (iter+1), "loss=", "{:.9f}".format(avg_loss))
+
+            # Computing the model accuracy
+            correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+            # Calculate accuracy on test data
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
 
 
 if __name__ == "__main__":
