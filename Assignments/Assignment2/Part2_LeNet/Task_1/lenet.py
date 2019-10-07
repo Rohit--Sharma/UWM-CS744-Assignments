e68119f82573c29b03a22ffe232416a5fe83f0cb
akshatabhat <mail.akshata18@gmail.com>
2019-10-07 16:38:20 -0500

    lenet for mnist - bug fixes

--- Assignments/Assignment2/Part2_LeNet/Task_1/lenet.py
+++ Assignments/Assignment2/Part2_LeNet/Task_1/lenet.py
@@ -1,26 +1,26 @@
 import tensorflow as tf
+import numpy as np
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D
+from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D, Flatten
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 # Convert to float and normalise it.
-train_images = train_images.astype('float32')/255
-test_images = test_images.astype('float32')/255
+train_images = train_images.astype(np.float32)/255
+test_images = test_images.astype(np.float32)/255
 
 #Reshape the training and test set
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
 test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
 # One-hot encoding of the labels
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
+train_labels = to_categorical(train_labels, 10)
+test_labels = to_categorical(test_labels, 10)
 
 model = Sequential()
-
 
 model.add(Conv2D(
 	filters=6, 
@@ -28,7 +28,7 @@ model.add(Conv2D(
 	strides=1,
 	padding="same",
 	activation="tanh",
-	input_shape = (32, 32, 1)
+	input_shape = (28, 28, 1)
 	))
 
 model.add(AveragePooling2D(
