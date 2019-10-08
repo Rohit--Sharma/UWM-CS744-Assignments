edfd6a8101af48fecaefeba1b3b39fa5ac16144c
akshatabhat <mail.akshata18@gmail.com>
2019-10-08 12:36:00 -0500

    Sync SGD

--- Assignments/Assignment2/Part1_LogisticRegression/Task_2/logistic_regression_sync.py
+++ Assignments/Assignment2/Part1_LogisticRegression/Task_2/logistic_regression_sync.py
@@ -88,7 +88,8 @@ def main():
                                    total_num_replicas=2)
             
             training_op = optimizer.minimize(loss, global_step=global_step)
-            hooks = [tf.train.StopAtStepHook(last_step=num_iter), optimizer.make_session_run_hook(is_chief, num_tokens=0)]
+            hooks = [optimizer.make_session_run_hook(is_chief, num_tokens=0)]
+            # tf.train.StopAtStepHook(last_step=num_iter), 
             # adding loss summary
             loss = tf.summary.scalar("loss", loss)
             merged = tf.summary.merge_all()
@@ -98,6 +99,7 @@ def main():
                 is_chief=is_chief,
                 config=config,
                 hooks=hooks,
+                stop_grace_period_secs=10,
                 checkpoint_dir="/tmp/train_logs")
             iter = 0
             
